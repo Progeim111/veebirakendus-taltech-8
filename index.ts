ba5626c4507e22231522c301bde4748a93a0cd54
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-05-30 09:55:43 +0300

    Failide loomine

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
+import stringsController from "./controllers/strings";
+
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
+
+app.use('/', stringsController);
+
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
