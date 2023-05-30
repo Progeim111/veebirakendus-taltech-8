884c8e981bcf2bc4239e1fe653c8b6f8b9aac4ba
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-05-30 10:55:44 +0300

    API päring teise rakendusse: Elering hindade kättesaamine

--- controllers/nordpool.ts
+++ controllers/nordpool.ts
@@ -4,10 +4,8 @@ import axios from "axios";
 const router: Router = Router();
 
 router.get("/nord-pool-price", async (req: Request, res: Response) => {
-    const response = await axios.get(
-        "https://dashboard.elering.ee/api/nps/price"
-    );
-    res.json(response.data);
+
+    res.json("TEST");
 });
 
 export default router;
--- index.ts
+++ index.ts
@@ -8,12 +8,16 @@ import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productListController from "./controllers/productlist";
 import parcelMachinesController from "./controllers/parcelmachines";
+import nordpoolController from "./controllers/nordpool";
 
 const app: Express = express();
 
-app.use(cors({
-    origin: ['http://localhost:3006']
-}));
+app.use(cors())
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
@@ -23,6 +27,7 @@ app.use('/', stringsController);
 app.use('/', productsController);
 app.use('/', productListController);
 app.use('/', parcelMachinesController);
+app.use('/', nordpoolController);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
