2cbc1a10f616103c118620609c8da33f16eef6a1
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-05-30 10:34:10 +0300

    Eesliidese implementeerimine

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
 
+import cors from "cors";
+
+
 import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productListController from "./controllers/productlist";
 
 const app: Express = express();
+
+app.use(cors())
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
@@ -14,6 +20,6 @@ app.use('/', stringsController);
 app.use('/', productsController);
 app.use('/', productListController);
 
-app.listen(3006,() => {
-    console.log(`[server]: Server is running at http://localhost:3006`);
+app.listen(3000,() => {
+    console.log(`[server]: Server is running at http://localhost:3000`);
 });
